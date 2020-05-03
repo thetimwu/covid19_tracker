97aed1677b0ad86b14e877b995bdf848cc79e0a4
tim wu <timwuuk@gmail.com>
2020-05-03 08:06:40 +0100

    basic project setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import Cards from "./components/Cards/Cards";
+// import Chart from "./components/Chart/Chart";
+// import CountryPicker from "./components/CountryPicker/CountryPicker";
+
+import { Cards, Chart, CountryPicker } from "./components";
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+};
+
+export default App;
